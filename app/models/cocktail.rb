125850311f892df31742cf4205b84063603c818d
Benoit Roualec <benoit.roualec@gmail.com>
2019-02-17 14:52:58 +0100

    Workin on prod problem

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  mount_uploader :photo, PhotoUploader
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  mount_uploader :photo, PhotoUploader
+  validates :name, presence: true, uniqueness: true
 end
